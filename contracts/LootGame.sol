384a6a306232537aaf80884859240eb60bbbe3c4
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-09-07 19:46:53 +0800

    complete basic function.

--- contracts/LootGame.sol
+++ contracts/LootGame.sol
@@ -1,69 +1,129 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-
-interface LootProject {
-
-    function getWeapon(uint256 tokenId) external view returns (string memory);
-
-    function getChest(uint256 tokenId) external view returns (string memory);
-
-    function getHead(uint256 tokenId) external view returns (string memory);
-
-    function getWaist(uint256 tokenId) external view returns (string memory);
-
-    function getFoot(uint256 tokenId) external view returns (string memory);
-
-    function getHand(uint256 tokenId) external view returns (string memory);
-
-    function getNeck(uint256 tokenId) external view returns (string memory);
-
-    function getRing(uint256 tokenId) external view returns (string memory);
-}
+import "./ILootProject.sol";
 
 contract LootGame {
     
     LootProject public loot;
 
+    IERC20 public rewardToken;
+
+    uint256 public rewardAmountPerRound;
+
+    uint256 public constant MAXROUND = 10;
+
+    uint256 public totalWarriors;
+
     struct Battle {
-        address challenger,
         address acceptor,
         uint256 challengerPower,
         uint256 acceptorPower,
-        uint256 happenedTime
+        uint256 happenedTime,
+        bool result;
     }
 
-    mapping ( address => Battle ) public BattleDetails;
+    struct Warriors {
+        address warriorAddress,
+        address originalNFT,
+        uint256 originalTokenId,
+        uint256 initialPower
+    }
 
+    mapping ( address => Battle ) public battleDetails;
 
-    mapping (address => mapping (uint256 => bool) ) candidateWarriors;
+    // record candidate warriors
+    mapping ( uint256 => Warriors ) public candidateWarriors;
 
+    mapping ( address => uint256 ) public currentIndex;
 
-    constructor (address _lootaddress) {
+    constructor (
+        address _lootaddress, 
+        address rewardTokenAddress
+        ) {
         loot = LootProject(_lootaddress);
+        rewardToken = IERC20(rewardTokenAddress);
     }
 
+    /** ========== external mutative functions ========== */
 
-
-    function battle() public {
+    function pvpBattle() external {
+        address warriorAddress = _msgSender();
+        require(currentIndex[warriorAddress] != 0, "pvpBattle: please register your role at first");
         
+        // get warriors message
+        uint256 warriorIndex = currentIndex[warriorAddress];
+        Warriors memory warrior = candidateWarriors[warriorIndex];
+
+        // select a random candidate warriors by challenger's basic power.
+        uint256 rand = uint256(abi.encodePacked(toString(warrior.initialPower)));
+        uint256 randomWarriorNumber = rand % totalWarriors;
+        Warriors memory acceptor = candidateWarriors[randomWarriorNumber];
+
+        // pvp battle
+        uint256 warriorPower = _calculateRandomScore(warrior.originalTokenId, warrior.initialPower);
+        uint256 acceptorPower = _calculateRandomScore(acceptor.originalTokenId, acceptor.initialPower);
+
+        battleDetails[warriorAddress].acceptor = acceptor;
+        battleDetails[warriorAddress].challengerPower = warriorPower;
+        battleDetails[warriorAddress].acceptorPower = acceptorPower;
+        battleDetails[warriorAddress].happenedTime = block.timestamp;
+
+        if(warriorPower > acceptorPower) {
+            battleDetails[warriorAddress].result = true;
+            
+            rewardToken.transfer(warriorAddress, rewardAmountPerRound);
+        } else {
+            battleDetails[warriorAddress].result = false;
+        }
+        
+        emit pvpBattled(warriorIndex, warriorAddress, acceptor.acceptorAddress, battleDetails[warriorAddress].result);
     }
 
 
-    function createRole(uint256 tokenId) public {
+    function registerRole(uint256 tokenId, uint256 rarityscore) external {
+
+        // transfer your fight token
         require(loot.transferFrom(_msgSender(), address(this), tokenId), "createRole: fail to create loot role");
+
+        uint256 nextWarrors = totalWarriors + 1;
+
+        candidateWarriors[nextWarrors].originalNFT = address(loot);
+        candidateWarriors[nextWarrors].originalTokenId = tokenid;
+        candidateWarriors[nextWarrors].initialPower = rarityscore;
+        candidateWarriors[nextWarrors].warriorAddress = _msgSender();
+        currentIndex[_msgSender()] = nextWarrors;
+
+        totalWarriors++;
+
+        emit roleRegistered(_msgSender(), tokenId, rarityscore);
     }
 
+    function quitFromGame(uint256 tokenId) external {
+
+        require(currentIndex[_msgSender()] == tokenId, "quitFromGame: Sorry, you do not a candidate warrior.");
+
+        // delete player records
+        delete currentIndex[_msgSender()];
+        delete candidateWarriors[tokenId];
+        totalWarriors--; 
+
+        // return player's token
+        loot.transfer(_msgSender(), tokenId);
+
+        emit roleQuit(tokenId, _msgSender());
+    }
 
 
     /** ========== internal view functions ========== */
 
+    // calculate random score basing on user's rarityscore of the nft
     function _calculateRandomScore(uint256 _rarityScore, uint256 tokenId) internal view returns (uint256) {
-        return randomScore = _getRandom(tokenId, _rarityScore) % _getScoreRange(_rarityScore);
-    }
-
-    function _getRandom(uint256 tokenId, uint256 _rarityScore) internal view returns (uint256) {
         uint256 rand = uint256(abi.encodepacked(toString(tokenId), toString(_rarityScore), toString(block.timestamp)));
+        (uint256 minScore, uint256 maxScore) = _getScoreRange(_rarityScore);
+        uint256 levelRange = maxScore - minScore;
+        
+        return randomScore = (rand % levelRange) + minScore;
     }
 
     function _getScoreRange(uint256 _rarityScore) internal view returns (uint256 minScore, uint256 maxScore) {
@@ -110,4 +170,13 @@ contract LootGame {
         }
         return string(buffer);
     }
+
+
+    /** ========== event ========== */
+
+    event roleRegistered(address indexed role, uint256 indexed tokenId, uint256 power);
+
+    event pvpBattled(uint256 indexed warriorIndex, address indexed warriorAddress, address acceptorAddress, bool result);
+
+    event roleQuit(uint256 indexed tokenId, address playerAddress);
 }
